51b80b407a9f14c222d9738ba34c88a9f55f62a4
jessicaappleton <appleton.jess@hotmail.com>
2020-11-13 09:57:45 +1100

    hole and nab

---
+++ holes/assets/js/main.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+	console.log('hello world');
+
+	$('.btn-bin').click(function(){
+		$('.bin').toggleClass('active');
+	});
+
+	$('.btn-food').click(function(){
+		$('.food').toggleClass('active');
+	});
+
+	$('.btn-geo').click(function(){
+		$('.geo').toggleClass('active');
+	});
+
+	$('.btn-earth').click(function(){
+		$('.earth').toggleClass('active');
+	});
+
+	$('.btn-mirror').click(function(){
+		$('.mirror').toggleClass('active');
+	});
+
+
+
+
+
+});
